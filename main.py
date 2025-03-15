374174d60795a8cb5439f4b6dd3fae728c5ab6ad
anasco119 <revo9techno@gmail.com>
2025-03-15 22:27:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -198,7 +198,7 @@ def main():
         listen="0.0.0.0",
         port=PORT,
         url_path=TOKEN,
-        webhook_url=f"https://join-bot.onrender.com/{TOKEN}"
+        webhook_url=f"https://joinbot-7mw7.onrender.com/{TOKEN}"
     )
 
 if __name__ == "__main__":
